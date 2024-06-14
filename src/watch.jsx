f793247dc25e0f210f316a69f977b4791ba5010c
Kalaranjani <kalaranjani.he@enchhash.com>
2024-06-14 16:27:36 -0400

    first commit

---
+++ src/watch.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0); 
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    if (running) {
+      timer = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else if (!running && time !== 0) {
+      clearInterval(timer);
+    }
+    return () => clearInterval(timer); 
+  }, [running, time]);
+
+  const startStopwatch = () => {
+    setRunning(true);
+  };
+
+  const stopStopwatch = () => {
+    setRunning(false);
+  };
+
+  const resetStopwatch = () => {
+    setRunning(false);
+    setTime(0);
+  };
+
+ 
+  const formatTime = (seconds) => {
+    const getSeconds = `0${seconds % 60}`.slice(-2);
+    const minutes = Math.floor(seconds / 60);
+    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getHours = `0${Math.floor(seconds / 3600)}`.slice(-2);
+
+    return `${getHours}:${getMinutes}:${getSeconds}`;
+  };
+
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>{formatTime(time)}</p>
+      <button onClick={startStopwatch}>Start</button>
+      <button onClick={stopStopwatch}>Stop</button>
+      <button onClick={resetStopwatch}>Reset</button>
+    </div>
+  );
+};
+
+export default Stopwatch;
